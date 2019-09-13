f3298d18d154a6cee1bfc12ab74a4dd62896ff4e
Miguel Prada <mapra99@gmail.com>
2019-09-13 15:26:48 -0500

    Created Microposts Resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_13_170802) do
+ActiveRecord::Schema.define(version: 2019_09_13_201810) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
