071328efe94b76a051c3d3094a07da0430ca9524
Miguel Prada <mapra99@gmail.com>
2019-09-13 17:48:23 -0500

    Added validation for user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
   #belongs_to :user
   has_many :microposts
+  validates :name,  format: { with: /\A[a-zA-Z|\s]+\z/, message: "must be only letters and spaces"},
+                    presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP },
+                    presence: true
 end
